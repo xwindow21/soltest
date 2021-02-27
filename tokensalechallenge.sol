c468dd365b569375b5f881764a3d68e921398f50
xwindow21 <xwindow@yeah.net>
2021-02-27 20:10:07 +0800

    modify

--- tokensalechallenge.sol
+++ tokensalechallenge.sol
@@ -9,7 +9,7 @@ contract TokenSaleChallenge {
     mapping(address => uint256) public balanceOf;
     uint256 constant PRICE_PER_TOKEN = 1 ether;
 
-    function TokenSaleChallenge(address _player) public payable {
+    function constractor(address _player) public payable {
         require(msg.value == 1 ether);
     }
 
